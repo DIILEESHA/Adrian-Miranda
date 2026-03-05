e639084abbccbe77808b1a02038698475c062f1a
DIILEESHA <dileeshanawarathna18@gmail.com>
2026-03-05 17:44:41 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,58 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/header/Header'
-import Intro from './components/intro/Intro'
-import Envelope from './components/env/Envelope'
-import Itinerary from './components/it/Itinerary'
-import Count from './components/count/Count'
-import Dresscode from './components/dress/Dresscode'
-import Story from './components/story/Story'
-import Faq from './components/faqs/Faq'
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from './components/header/Header';
+import Intro from './components/intro/Intro';
+import Envelope from './components/env/Envelope';
+import Itinerary from './components/it/Itinerary';
+import Count from './components/count/Count';
+import Dresscode from './components/dress/Dresscode';
+import Story from './components/story/Story';
+import Faq from './components/faqs/Faq';
 import Party from './pages/party/Party';
 import Nav from './components/nav/Nav';
 import All from './components/faqs/All';
 import Things from './components/things/Things';
-import heys from "./assets/hey.jpg"
+import heys from "./assets/hey.jpg";
 import Reg from './components/reg/Reg';
 import Alls from './pages/schedule/Alls';
 import Travels from './pages/travel/Travels';
+import Pwd from './Pwd'; // import the password page
+
 const App = () => {
+  const [unlocked, setUnlocked] = useState(false);
+
+  if (!unlocked) {
+    // Show password page first
+    return <Pwd onCorrect={() => setUnlocked(true)} />;
+  }
+
+  // Once unlocked, render the full site
   return (
     <Router>
-<Nav/>
+      <Nav />
       <Routes>
-        <Route path="/" element={
-          <>
-          <Header title="SEPTEMBER 12, 2026  -  NEW YORK, NY"/>
-            <Intro />
-            <Envelope />
-            <Itinerary />
-            <div className="hey">
-              <img src={heys} alt="" className="hey_img" />
-            </div>
-            <Things/>
-            <Count />
-            <Reg/>
-            <Dresscode />
-            <Story />
-            <Faq />
-          </>
-        } />
+        <Route
+          path="/"
+          element={
+            <>
+              <Header title="SEPTEMBER 12, 2026  -  NEW YORK, NY" />
+              <Intro />
+              <Envelope />
+              <Itinerary />
+              <div className="hey">
+                <img src={heys} alt="" className="hey_img" />
+              </div>
+              <Things />
+              <Count />
+              <Reg />
+              <Dresscode />
+              <Story />
+              <Faq />
+            </>
+          }
+        />
 
-        {/* Example of another page */}
+        {/* Example of other pages */}
         <Route path="/wedding-party" element={<All />} />
         <Route path="/travel-info" element={<Travels />} />
         <Route path="/wedding-schedule" element={<Alls />} />
@@ -48,7 +61,7 @@ const App = () => {
 
       <h2 className="footer">Adrian & Miranda © All Rights Reserved</h2>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/Pwd.jsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from "react";
+import ams from "./assets/am.png";
+import "./index.css";
+
+const Pwd = ({ onCorrect }) => {
+  const [password, setPassword] = useState("");
+  const correctPassword = "gohange"; // Set your password
+
+  // Check localStorage on mount
+  useEffect(() => {
+    const unlocked = localStorage.getItem("siteUnlocked");
+    if (unlocked === "true") {
+      onCorrect(); // Already unlocked
+    }
+  }, [onCorrect]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (password === correctPassword) {
+      localStorage.setItem("siteUnlocked", "true"); // Store unlocked state
+      onCorrect(); // Unlock the site
+    } else {
+      alert("Incorrect password!");
+    }
+  };
+
+  return (
+    <div className="pwd">
+      <div className="gfd">
+        <div className="pwd_card">
+          <div className="amz">
+            <div className="amzz">
+              <img src={ams} alt="" className="am_ongs" />
+            </div>
+            <form className="amz" onSubmit={handleSubmit}>
+              <input
+                type="password"
+                placeholder="Enter the password"
+                className="am_input"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <button type="submit" className="enter">
+                Enter
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Pwd;
